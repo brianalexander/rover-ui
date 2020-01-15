a91ae97eecaa03279b71b7ad111e915f654dad65
Brian Alexander <brianalexander28@gmail.com>
2020-01-14 19:12:15 -0800

    react-3d-viewer won't work

--- src/components/IMU.js
+++ src/components/IMU.js
@@ -1,11 +1,29 @@
 import React, { Component } from "react";
-import { OBJModel } from "react-3d-viewer";
+import { Tick, OBJModel } from "react-3d-viewer";
 
 class IMU extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      position: { x: -40, y: -25, z: -35 },
+      // position: { x: 0, y: -0, z: -0 },
+      rotation: { x: 0, y: 0, z: 0 }
+    };
+
+    // setInterval(() => {
+    //   let rotation = { ...this.state.rotation };
+    //   rotation.y++;
+    //   this.setState({ rotation: rotation });
+    // }, 1000);
+  }
+
+  componentDidMount() {
+    this.tick = Tick(() => {
+      let rotation = { ...this.state.rotation };
+      rotation.y += 0.005;
+      this.setState({ rotation: rotation });
+    });
   }
 
   render() {
@@ -17,7 +35,8 @@ class IMU extends Component {
           height={500}
           texPath=""
           src="./rover-model.obj"
-          position={{ x: -40, y: -25, z: -35 }}
+          position={this.state.position}
+          rotation={this.state.rotation}
         />
       </div>
     );
